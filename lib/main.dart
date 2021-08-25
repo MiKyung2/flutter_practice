d0b1406df5c8c97674a0c099dbcd70df1c17af8b
jmk2 <wlgurdlcjswo@naver.com>
2021-08-25 23:53:51 +0900

    순한맛 강좌 8 완료

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'First App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
     );
   }
-}{
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text("Hello"),
+            Text("Hello"),
+            Text("Hello"),
+        ],),
+      )
+    );
+  }
 }
