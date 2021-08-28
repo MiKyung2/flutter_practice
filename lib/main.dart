a6f0c2e850657722519e7e8f63187a4559749024
jmk2 <wlgurdlcjswo@naver.com>
2021-08-29 01:00:16 +0900

    미션 화면 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,48 +10,206 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "BBANTO",
-      home: Grade(),
+      title: 'mayple',
+      theme: ThemeData(
+        fontFamily: 'Noto_Serif_KR',
+      ),
+      home: Mayple(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  const Grade({ Key? key }) : super(key: key);
+class Mayple extends StatelessWidget {
+  const Mayple({ Key? key }) : super(key: key);
+
+  final double commonPadding = 20.0;
+
+  Widget _featureBox({context: BuildContext, title: String, description: String}) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.50,
+      decoration: BoxDecoration(
+      border: Border.all(
+        width: 1,
+        color: Colors.grey
+      )),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(
+            commonPadding, 10.0, commonPadding, 10.0),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+          children: const [
+            Text('EXPERIENCE',
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.blueGrey)
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('8 years',
+              style: TextStyle(
+                fontSize: 16.0,
+              )
+            ),
+          ],
+        ),
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text("BBANTO"),
-        backgroundColor: Colors.amber[700],
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("NAME",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            )),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text("BBANTO",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ))
-          ],
+        title: Text(
+          'mayple',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold
+          ),
         ),
-      )
+        backgroundColor: Color(0xffF7F1E8),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(
+              Icons.menu,
+              color: Colors.redAccent,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 9,
+            child:  Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/jjh.jpg"), 
+                  fit: BoxFit.contain
+                )
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(commonPadding),
+                child:
+                  Text("Find your industry's\ntop vetted marketing\nexperts",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w800,
+                    )),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(commonPadding),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Stefan Garlson',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )),
+                            SizedBox(
+                              height: 14,
+                            ),
+                            Row(children: const [
+                              Text(
+                                '4.8',
+                                style: TextStyle(
+                                  color: Colors.redAccent, fontSize: 18),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 2),
+                                child: Icon(
+                                  Icons.star,
+                                  color: Colors.redAccent,
+                                )),
+                              Padding(
+                                padding: EdgeInsets.only(right: 2),
+                                child: Icon(
+                                  Icons.star,
+                                  color: Colors.redAccent,
+                                )),
+                              Padding(
+                                  padding: EdgeInsets.only(right: 2),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.redAccent,
+                                  )),
+                              Padding(
+                                  padding: EdgeInsets.only(right: 2),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.redAccent,
+                                  )),
+                              Padding(
+                                  padding: EdgeInsets.only(right: 2),
+                                  child: Icon(
+                                    Icons.star_half,
+                                    color: Colors.redAccent,
+                                  ))
+                            ])
+                          ]
+                        )
+                      ],
+                    ),
+                  )
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          _featureBox(
+                            context: context,
+                            title: 'MARKETING EXPERTISE:', 
+                            description: 'Tech expert'
+                          ),
+                          _featureBox(
+                            context: context,
+                            title: 'EXPERIENCE',
+                            description: '8 years'
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          _featureBox(
+                            context: context,
+                            title: 'WORKED WITH:',
+                            description: 'Jolt, Monday,\nSalesforce'
+                          ),
+                          _featureBox(
+                            context: context,
+                            title: 'AVG, INCREASE IN SALES:',
+                            description: '+146%'
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          )
+        ],
+      ),
     );
   }
 }
